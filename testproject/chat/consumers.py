07c2238bce37c852d5c03d60e20e5ee02d9b2cf2
Topi Saavalainen <topi.saavalainen@edu.turku.fi>
2025-01-24 13:46:36 +0200

    Initial commit

---
+++ testproject/chat/consumers.py
@@ -0,0 +1,33 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.group_name = "chat"
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name,
+            self.channel_name
+        )
+        self.accept()
+
+    def disconnect(self):
+        pass
+
+    def receive(self, text_data):
+        json_data = json.loads(text_data)
+        message = json_data['message']
+        async_to_sync(self.channel_layer.group_send)(
+            self.group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    def chat_message(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps({
+            'type': 'chat',
+            'message': message
+        }))
